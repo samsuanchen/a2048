9449522ab6c570a3ab3dc884c3820557a1bc9299
samsuanchen <samsuanchen@gmail.com>
2015-01-07 19:45:53 +0800

    first commit

---
+++ ksana.js
@@ -0,0 +1,11 @@
+jsonp_handler({
+"version": "1",
+"build": 1,
+"title":"a2048",
+"minruntime": 4,
+"baseurl":"http://samsuanchen@rawgit.com/samsuanchen/a2048",
+"description":"",
+"files":
+  ["index.html","build.js","build.css","jquery.js","react-with-addons.js","ksana.js",
+  "sample.kdb"]
+})
